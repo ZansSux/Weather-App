23e53684733c479ed11b5089441695f8dd907ad3
) <milankc@MacBook.local>
2026-06-02 13:46:12 +0545

    fix

--- request_example.py
+++ request_example.py
@@ -1,7 +1,12 @@
 import requests
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=27.7&longitude=85.3&current_weather=true"
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 27.7172,
+    "longitude": 85.3240,
+    "current_weather": True
+}
 
-response = requests.get(url)
+response = requests.get(url, params=params)
 data = response.json()
 print(f"The temperature at latitude:{data['latitude']}, longitude:{data['longit gitude']} is {data['current_weather']['temperature']}.")
