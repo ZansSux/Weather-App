0cf74d29185623198970d65b421c82e96ce7f27b
) <milankc@MacBook.local>
2026-06-05 15:55:00 +0545

    GUI using Tkinter

--- request_example.py
+++ request_example.py
@@ -10,40 +10,30 @@ def getcity(CityName):
     }
     return params
 
-def getCoordinates():
-    cc = input("Enter city to search: ")
+def getCoordinates(cc):
     response = requests.get(url1, params=getcity(cc))
     data = response.json()
     if 'results' not in data:
         print(f"Error: city: '{cc}' could not be found.")
         return None
+    
     first_match = data['results'][0]
-    coor = [first_match['latitude'], first_match['longitude']]
+
+    coor = [first_match['latitude'], first_match['longitude'], first_match['name']]
     return coor
     
     
-def weatherout():
-    coors = getCoordinates()
+def weatherout(cc):
+    coors = getCoordinates(cc)
     param1 = {
         "latitude": coors[0],
         "longitude": coors[1],
         "current": "temperature_2m,wind_speed_10m,wind_direction_10m"
     }
+    cityy = coors[2]
     response = requests.get(url2, params=param1)
     data = response.json()
 
     current_weather = data['current']
-
-    print(('=')*40)
-    print(("WEATHER DETAILS").center(40,"*"))
-    print(('=')*40)
-    print(f'''Temperature: {current_weather['temperature_2m']}°C
-Wind Speed: {current_weather['wind_speed_10m']} km/h
-Wind Direction: {current_weather['wind_direction_10m']}°''')
-print(('=')*40)
-print(("CITY SEARCH").center(40,"*"))
-print(('=')*40)
-weatherout()
-
-
+    return current_weather, cityy
 
